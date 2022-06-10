2bfd3f6467f2bff6ac0d1136c783ece5102008ca
Vinicius Lucena <jviniiciuslucena@gmail.com>
2022-06-10 13:27:55 -0300

    Back end of mycontacts finished

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+require('express-async-errors');
+
+const routes = require('./routes');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+app.use(express.json());
+app.use(routes);
+// eslint-disable-next-line no-unused-vars
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000 🔥'));
