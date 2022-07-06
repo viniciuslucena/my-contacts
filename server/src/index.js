7767a0e56cc14fe3cc633a5257c663119b0dd6f5
Vinicius Lucena <jviniiciuslucena@gmail.com>
2022-07-06 20:02:29 -0300

    feat: headers on api

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,15 @@
 const express = require('express');
 require('express-async-errors');
 
+const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 app.use(routes);
-// eslint-disable-next-line no-unused-vars
-app.use((error, request, response, next) => {
-  console.log(error);
-  response.sendStatus(500);
-});
+app.use(errorHandler);
 
-app.listen(3000, () => console.log('Server started at http://localhost:3000 🔥'));
+app.listen(3001, () => console.log('Server started at http://localhost:3001 🔥'));
