389cdfd77749b051a15e281ae088b16bdf19f680
Vinicius Lucena <jviniiciuslucena@gmail.com>
2022-07-20 16:58:52 -0300

    feat: HttpClient service

--- front/src/services/ContactsService.js
+++ front/src/services/ContactsService.js
@@ -1,8 +1,17 @@
+import HttpClient from './utils/HttpClient';
+
 class ContactsService {
-  async listContacts(orderBy = 'asc') {
-    const response = await fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`);
-    return response.json();
+  constructor() {
+    this.httpClient = new HttpClient('http://localhost:3001');
   }
+
+  async listContacts(orderBy = 'asc') {
+    return this.httpClient.get(`/contacts?orderBy=${orderBy}`);
+  }
+
+  // async createContact(contact) {
+  //   return HttpClient.post(`/contact`, contact);
+  // }
 }
 
 export default new ContactsService();
