493398ea0616bc64c59daef41de7d4923bd9c933
Vinicius Lucena <jviniiciuslucena@gmail.com>
2022-08-02 17:43:01 -0300

    feat: body parser based on content type

--- front/src/services/utils/HttpClient.js
+++ front/src/services/utils/HttpClient.js
@@ -6,11 +6,20 @@ class HttpClient {
   async get(path) {
     const response = await fetch(`${this.baseURL}${path}`);
 
-    if (response.ok) {
-      return response.json();
+    const contentType = response.headers.get('Content-Type');
+
+    let body = null;
+    if (contentType.includes('application/json')) {
+      body = await response.json();
     }
 
-    throw new Error(`${response.status} - ${response.statusText}`);
+    if (response.ok) {
+      return body;
+    }
+
+    throw new Error(
+      body?.error || `${response.status} - ${response.statusText}`,
+    );
   }
 }
 
